83b3e14a16a1fda6ded7ee8cfce0a428bbb85911
mansdeanna2-collab <mansdeanna2@gmail.com>
2026-02-24 04:17:58 +0800

    Update import_to_mysql.py

--- scripts/import_to_mysql.py
+++ scripts/import_to_mysql.py
@@ -37,7 +37,7 @@ DB_CONFIG = {
     "host": os.environ.get("DB_HOST", "127.0.0.1"),
     "port": int(os.environ.get("DB_PORT", "3306")),
     "user": os.environ.get("DB_USER", "yopao"),
-    "password": os.environ.get("DB_PASS", "6ffc39ea3c3a285d"),
+    "password": os.environ.get("DB_PASS", "LmczwhREFaEdF8FK"),
     "database": os.environ.get("DB_NAME", "yopao"),
     "charset": "utf8mb4",
 }
